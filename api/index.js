3129ad65252caf7e1c55572b964be767294fe235
abhishekhojha <abhishekhojha45@gmail.com>
2025-05-08 16:36:31 +0530

    completed6

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(
   cors({
     origin: "https://appointment-mannager-mxpertz-hvve.vercel.app", // frontend URL
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
